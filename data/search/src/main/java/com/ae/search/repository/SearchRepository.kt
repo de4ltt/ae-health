9ef812e4c6994700514ebc8049000febffdb09de
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-05 17:49:14 +0300

    feat(data:search): add SearchRepository functionality

--- data/search/src/main/java/com/ae/search/repository/SearchRepository.kt
+++ data/search/src/main/java/com/ae/search/repository/SearchRepository.kt
@@ -1,12 +1,28 @@
 package com.ae.search.repository
 
+import com.ae.network.ISearchDataSource
+import com.ae.network.dto.TypedItemsDTO
+import com.ae.network.model.NetworkRequestResult
+import com.ae.search.mapper.toDomain
+import com.ae.search.mapper.toNetwork
 import com.ae.search.model.ISearchItem
 import com.ae.search.model.SearchParams
 import kotlinx.coroutines.flow.Flow
 
-class SearchRepository() : ISearchRepository {
+class SearchRepository(
+    private val searchDataSource: ISearchDataSource
+) : ISearchRepository {
 
-    override fun search(searchParams: SearchParams): Flow<List<ISearchItem>> {
+    override suspend fun searchWithFilters(searchParams: SearchParams): List<ISearchItem> {
+        val response = searchDataSource.searchWithFilters(searchParams.toNetwork())
+
+        if (response is NetworkRequestResult.Success<List<TypedItemsDTO>>)
+            return response.data.toDomain()
+        else
+            throw ((response as NetworkRequestResult.Error).error)
+    }
+
+    override suspend fun searchNearby(searchParams: SearchParams): List<ISearchItem> {
         TODO("Not yet implemented")
     }
 }
