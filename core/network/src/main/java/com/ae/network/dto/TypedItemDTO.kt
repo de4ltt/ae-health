5f9d0b1c188240f682c95828fe3cace1ba47c34f
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-05 17:46:16 +0300

    feat(core:network): add TypedItemDTO

---
+++ core/network/src/main/java/com/ae/network/dto/TypedItemDTO.kt
@@ -0,0 +1,9 @@
+package com.ae.network.dto
+
+data class TypedItemDTO(
+    val title: String,
+    val subtitle: String?,
+    val category: String,
+    val image: String?,
+    val link: String
+)
