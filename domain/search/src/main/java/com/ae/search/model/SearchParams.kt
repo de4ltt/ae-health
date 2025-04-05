4f1da9ec0e0f20a3d463fadb2340df6bf2466d3a
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-05 17:48:05 +0300

    refactor(domain:search): reposition the params

--- domain/search/src/main/java/com/ae/search/model/SearchParams.kt
+++ domain/search/src/main/java/com/ae/search/model/SearchParams.kt
@@ -3,7 +3,7 @@ package com.ae.search.model
 data class SearchParams(
     val query: String,
     val itemsFilters: List<SearchItemCategory>,
+    val radius: Int?,
     val lat: Double,
-    val lon: Double,
-    val radius: Int
+    val lon: Double
 )
