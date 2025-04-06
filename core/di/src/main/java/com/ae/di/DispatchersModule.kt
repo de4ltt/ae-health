1023bcde14dfcd2b8b9a0579b5ad99e3985a42f9
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-06 21:11:33 +0300

    feat(core:di): add DispatchersModule

---
+++ core/di/src/main/java/com/ae/di/DispatchersModule.kt
@@ -0,0 +1,19 @@
+package com.ae.di
+
+import com.ae.annotations.DefaultDispatcher
+import com.ae.annotations.IoDispatcher
+import dagger.Provides
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+abstract class DispatchersModule {
+
+    @IoDispatcher
+    @Provides
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @DefaultDispatcher
+    @Provides
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+}
