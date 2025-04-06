11672ba3fd0e2cf6380e500762fcd78e37c29842
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-06 18:58:14 +0300

    feat(core:network): add NetworkRequestHandler

---
+++ core/network/src/main/java/com/ae/network/data_source/NetworkRequestHandler.kt
@@ -0,0 +1,26 @@
+package com.ae.network.data_source
+
+import com.ae.network.model.NetworkRequestError
+import com.ae.network.model.NetworkRequestResult
+import retrofit2.Response
+
+internal suspend fun <T> handleNetworkRequest(
+    request: suspend () -> Response<T>
+): NetworkRequestResult<T> = try {
+    val response = request()
+
+    if (response.body() == null)
+        throw Exception("Body is null")
+
+    if (response.isSuccessful)
+        NetworkRequestResult.Success(response.body()!!)
+    else NetworkRequestResult.Error(
+        error = when (response.code()) {
+            in 400..499 -> NetworkRequestError.ClientError(response.code().toString())
+            in 500..599 -> NetworkRequestError.ServerError(response.code().toString())
+            else -> NetworkRequestError.UnknownError(response.code().toString())
+        }
+    )
+} catch (e: Throwable) {
+    NetworkRequestResult.Error(NetworkRequestError.UnknownError(e.message))
+}
