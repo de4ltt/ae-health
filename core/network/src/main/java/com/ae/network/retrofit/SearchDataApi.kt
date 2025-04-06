0ace999d8e33eae76eebaa2f96883fa41d230488
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-06 18:59:15 +0300

    feat(core:network): make internal SearchDataApi

--- core/network/src/main/java/com/ae/network/retrofit/SearchDataApi.kt
+++ core/network/src/main/java/com/ae/network/retrofit/SearchDataApi.kt
@@ -1,17 +1,16 @@
 package com.ae.network.retrofit
 
-import com.ae.network.dto.TypedItemsDTO
+import com.ae.network.dto.retrofit.TypedItemsResponse
 import retrofit2.Response
-import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 private const val DEFAULT_TOWN = "krasnodar"
 
-interface SearchDataApi {
-    @POST
+internal interface SearchDataApi {
+    @POST("search")
     fun searchWithFilters(
         @Query("query") query: String,
         @Query("town") town: String = DEFAULT_TOWN
-    ): Response<List<TypedItemsDTO>>
+    ): Response<List<TypedItemsResponse>>
 }
