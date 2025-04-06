00aba76633bb743f778a424738a35a3febcf01af
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-06 19:00:14 +0300

    feat(core:network): add SearchItemCategory enum

---
+++ core/network/src/main/java/com/ae/network/model/SearchItemCategory.kt
@@ -0,0 +1,10 @@
+package com.ae.network.model
+
+import com.ae.network.jsoup.IJsoupApi
+import kotlin.jvm.functions.FunctionN
+
+enum class SearchItemCategory {
+    DOCTOR,
+    LPU,
+    SERVICES
+}
