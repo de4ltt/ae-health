ed7032e1ec231725787cbdd05933338a3bf7040c
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-06 18:59:31 +0300

    feat(core:network): make internal SearchDataNearbyApi

--- core/network/src/main/java/com/ae/network/retrofit/SearchDataNearbyApi.kt
+++ core/network/src/main/java/com/ae/network/retrofit/SearchDataNearbyApi.kt
@@ -1,11 +1,13 @@
 package com.ae.network.retrofit
 
+import com.ae.network.dto.retrofit.FeatureCollectionResponse
+import retrofit2.Response
 import retrofit2.http.POST
 import retrofit2.http.Query
 
-interface SearchDataNearbyApi {
+internal interface SearchDataNearbyApi {
     @POST
     fun searchNearby(
         @Query("bbox") bbox: String
-    ): List<>
+    ): Response<FeatureCollectionResponse>
 }
