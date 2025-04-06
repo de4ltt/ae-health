a123d90503cdea7b0a2a2effe418a105d182a6c0
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-06 19:01:47 +0300

    feat(data:search): make SearchRepository internal

--- data/search/src/main/java/com/ae/search/repository/SearchRepository.kt
+++ data/search/src/main/java/com/ae/search/repository/SearchRepository.kt
@@ -1,28 +1,32 @@
 package com.ae.search.repository
 
 import com.ae.network.ISearchDataSource
-import com.ae.network.dto.TypedItemsDTO
+import com.ae.network.dto.retrofit.TypedItemResponse
 import com.ae.network.model.NetworkRequestResult
 import com.ae.search.mapper.toDomain
 import com.ae.search.mapper.toNetwork
 import com.ae.search.model.ISearchItem
 import com.ae.search.model.SearchParams
-import kotlinx.coroutines.flow.Flow
 
-class SearchRepository(
+internal class SearchRepository(
     private val searchDataSource: ISearchDataSource
 ) : ISearchRepository {
 
     override suspend fun searchWithFilters(searchParams: SearchParams): List<ISearchItem> {
         val response = searchDataSource.searchWithFilters(searchParams.toNetwork())
 
-        if (response is NetworkRequestResult.Success<List<TypedItemsDTO>>)
+        if (response is NetworkRequestResult.Success<List<TypedItemResponse>>)
             return response.data.toDomain()
         else
             throw ((response as NetworkRequestResult.Error).error)
     }
 
     override suspend fun searchNearby(searchParams: SearchParams): List<ISearchItem> {
-        TODO("Not yet implemented")
+        val response = searchDataSource.searchNearbyWithFilters(searchParams.toNetwork())
+
+        if (response is NetworkRequestResult.Success<List<TypedItemResponse>>)
+            return response.data.toDomain()
+        else
+            throw ((response as NetworkRequestResult.Error).error)
     }
 }
