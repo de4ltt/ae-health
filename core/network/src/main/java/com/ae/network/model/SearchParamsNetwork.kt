c2b37bb603f6ba37717ccd59c5afa63c121a27be
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-06 19:00:51 +0300

    refactor(core:network): change type itemFilters to SearchItemCategory in SearchParamsNetwork

--- core/network/src/main/java/com/ae/network/model/SearchParamsNetwork.kt
+++ core/network/src/main/java/com/ae/network/model/SearchParamsNetwork.kt
@@ -2,7 +2,7 @@ package com.ae.network.model
 
 data class SearchParamsNetwork(
     val query: String,
-    val itemsFilters: List<String>,
+    val itemFilters: List<SearchItemCategory>,
     val radius: Int?,
     val lat: Double,
     val lon: Double
