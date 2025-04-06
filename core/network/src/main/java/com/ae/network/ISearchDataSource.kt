bd385f6850da955c84b8708c63d5a8e90cf6a86b
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-06 18:55:58 +0300

    refactor(core:network): change function name in ISearchDataSource

--- core/network/src/main/java/com/ae/network/ISearchDataSource.kt
+++ core/network/src/main/java/com/ae/network/ISearchDataSource.kt
@@ -1,10 +1,12 @@
 package com.ae.network
 
-import com.ae.network.dto.TypedItemsDTO
+import com.ae.network.dto.retrofit.TypedItemResponse
+import com.ae.network.dto.retrofit.TypedItemsResponse
+import com.ae.network.model.CoordinatedArea
 import com.ae.network.model.NetworkRequestResult
 import com.ae.network.model.SearchParamsNetwork
 
 interface ISearchDataSource {
-    suspend fun searchWithFilters(searchParams: SearchParamsNetwork): NetworkRequestResult<List<TypedItemsDTO>>
-    suspend fun searchNearby(searchParams: SearchParamsNetwork): NetworkRequestResult<List<TypedItemsDTO>>
+    suspend fun searchWithFilters(searchParams: SearchParamsNetwork): NetworkRequestResult<List<TypedItemResponse>>
+    suspend fun searchNearbyWithFilters(searchParams: SearchParamsNetwork): NetworkRequestResult<List<TypedItemResponse>>
 }
