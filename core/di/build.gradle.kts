40de3b3751c90be8b0e4d3bc6f48179a895ddac8
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-06 20:46:26 +0300

    build(core:di): add dagger dependency

--- core/di/build.gradle.kts
+++ core/di/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
+    id("com.google.devtools.ksp")
 }
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -10,4 +11,9 @@ kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
+}
+
+dependencies {
+    ksp(libs.dagger.compiler)
+    implementation(libs.dagger)
 }
