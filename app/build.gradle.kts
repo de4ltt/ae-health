235478886dc5eb1027ae371f2db18b2bff6de8dd
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-06 19:09:57 +0300

    build(app): add ksp and dagger dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -58,4 +59,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    ksp(libs.dagger.compiler)
+    implementation(libs.dagger)
 }
