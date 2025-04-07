e6fac86dfd5462105fad7e94074a883ead2f963d
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-07 22:32:39 +0300

    feat(feature:home): add SearchViewModelFactory

---
+++ feature/home/src/main/java/com/ae/home/viewmodel/SearchViewModelFactory.kt
@@ -0,0 +1,22 @@
+package com.ae.home.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.ae.annotations.DefaultDispatcher
+import com.ae.search.use_case.ISearchWithFiltersUseCase
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+
+class SearchViewModelFactory @Inject constructor(
+    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher,
+    private val searchWithFiltersUseCase: ISearchWithFiltersUseCase
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SearchViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return SearchViewModel(defaultDispatcher, searchWithFiltersUseCase) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
+}
