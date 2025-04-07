fb49ad698bdbe2041e01591964297e6af965bc3e
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-07 22:34:00 +0300

    refactor(core:network): remove @Inject from SecretProperties

--- core/network/src/main/java/com/ae/network/model/SecretProperties.kt
+++ core/network/src/main/java/com/ae/network/model/SecretProperties.kt
@@ -2,7 +2,7 @@ package com.ae.network.model
 
 import javax.inject.Inject
 
-internal class SecretProperties @Inject constructor(
+internal class SecretProperties(
     override val apiBaseUri: String = "https://prodoctorov.ru/api/",
     override val mapBaseUri: String = "https://prodoctorov.ru/ajax/map/yamap_get_json/",
     override val defaultUri: String = "https://prodoctorov.ru"
