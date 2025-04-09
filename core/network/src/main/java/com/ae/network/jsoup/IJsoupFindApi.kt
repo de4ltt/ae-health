b0ba2b841e95bcd84098e6ff343a1d5844dbd2f6
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-09 22:11:22 +0300

    refactor(core:network): add typealias FoundElements into IJsoupFindApi::find

--- core/network/src/main/java/com/ae/network/jsoup/IJsoupFindApi.kt
+++ core/network/src/main/java/com/ae/network/jsoup/IJsoupFindApi.kt
@@ -1,10 +1,13 @@
 package com.ae.network.jsoup
 
 import com.ae.network.dto.retrofit.TypedItemResponse
+import com.ae.network.jsoup.`typealias`.FoundElements
+import com.ae.network.model.SearchParamsNetwork
+import com.ae.network.request_result.NetworkRequestResult
 
 interface IJsoupFindApi {
-    suspend fun find(query: String): List<TypedItemResponse>
-    suspend fun findClinics(query: String): List<TypedItemResponse>
-    suspend fun findServices(query: String): List<TypedItemResponse>
-    suspend fun findDoctors(query: String): List<TypedItemResponse>
+    suspend fun find(query: SearchParamsNetwork): NetworkRequestResult<FoundElements>
+    suspend fun findClinics(query: String): NetworkRequestResult<List<TypedItemResponse>>
+    suspend fun findServices(query: String): NetworkRequestResult<List<TypedItemResponse>>
+    suspend fun findDoctors(query: String): NetworkRequestResult<List<TypedItemResponse>>
 }
