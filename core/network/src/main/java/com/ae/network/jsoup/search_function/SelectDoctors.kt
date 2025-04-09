f46c801ef4bf17eeeef90e4c96857af2aa6ba2c8
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-09 22:08:22 +0300

    feat(core:network): add SelectDoctors function

---
+++ core/network/src/main/java/com/ae/network/jsoup/search_function/SelectDoctors.kt
@@ -0,0 +1,31 @@
+package com.ae.network.jsoup.search_function
+
+import com.ae.network.dto.retrofit.TypedItemResponse
+import org.jsoup.nodes.Document
+
+internal suspend fun selectDoctors(
+    doc: Document
+): Pair<String, List<TypedItemResponse>>? {
+
+    val doctorElements = doc.select("article.b-card.b-card_list-item.b-section-box__elem")
+
+    val results: MutableList<TypedItemResponse> = mutableListOf()
+
+    for (doctorElement in doctorElements) {
+        val doctor = TypedItemResponse(
+            title = doctorElement.selectFirst("a.b-card__name-link.b-link.ui-text.ui-text_h6.ui-kit-color-text")!!
+                .text(),
+            subtitle = doctorElement.selectFirst("p.b-card__category.ui-text.ui-text_body-1.ui-kit-color-text-secondary")!!
+                .text(),
+            image = doctorElement.selectFirst("img.b-card__avatar-img")!!.attr("src"),
+            link = doctorElement.selectFirst("a.b-card__name-link.b-link.ui-text.ui-text_h6.ui-kit-color-text")!!
+                .attr("href"),
+        )
+
+        results += doctor
+    }
+
+    return if (results.isNotEmpty()) {
+        Pair("DOCTOR", results)
+    } else null
+}
