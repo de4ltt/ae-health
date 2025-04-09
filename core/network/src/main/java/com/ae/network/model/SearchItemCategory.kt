3632a39b56fa3b0e970e13d04605d1b66348db6a
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-09 22:09:15 +0300

    feat(core:network): add selectFunction reference to SearchItemCategory

--- core/network/src/main/java/com/ae/network/model/SearchItemCategory.kt
+++ core/network/src/main/java/com/ae/network/model/SearchItemCategory.kt
@@ -3,10 +3,16 @@ package com.ae.network.model
 import com.ae.network.jsoup.search_function.searchForDoctor
 import com.ae.network.jsoup.search_function.searchForLpu
 import com.ae.network.jsoup.search_function.searchForServices
+import com.ae.network.jsoup.search_function.selectDoctors
+import com.ae.network.jsoup.search_function.selectLpus
 import com.ae.network.jsoup.`typealias`.SearchFunction
+import com.ae.network.jsoup.`typealias`.SelectFunction
 
-enum class SearchItemCategory(internal val searchFunction: SearchFunction) {
-    DOCTOR(::searchForDoctor),
-    LPU(::searchForLpu),
-    SERVICES(::searchForServices)
+enum class SearchItemCategory(
+    internal val searchFunction: SearchFunction,
+    internal val selectFunction: SelectFunction
+) {
+    DOCTOR(searchFunction = ::searchForDoctor, selectFunction = ::selectDoctors),
+    LPU(searchFunction = ::searchForLpu, selectFunction = ::selectLpus),
+    SERVICES(searchFunction = ::searchForServices, selectFunction = ::selectLpus)
 }
