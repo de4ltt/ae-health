456b98067e3ad126aa2aa4ddc01811f2f5c58ae6
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-09 20:53:28 +0300

    refactor(core:network): remove redundant imports from SearchItemCategory

--- core/network/src/main/java/com/ae/network/model/SearchItemCategory.kt
+++ core/network/src/main/java/com/ae/network/model/SearchItemCategory.kt
@@ -1,114 +1,12 @@
 package com.ae.network.model
 
-import com.ae.network.dto.retrofit.TypedItemResponse
-import com.ae.network.jsoup.IJsoupMapApi
-import com.ae.network.request_result.NetworkRequestResult
-
-internal typealias SearchFunction =
-        suspend (SearchParamsNetwork, List<String>, IJsoupMapApi) -> List<TypedItemResponse>
+import com.ae.network.jsoup.search_function.searchForDoctor
+import com.ae.network.jsoup.search_function.searchForLpu
+import com.ae.network.jsoup.search_function.searchForServices
+import com.ae.network.jsoup.`typealias`.SearchFunction
 
 enum class SearchItemCategory(internal val searchFunction: SearchFunction) {
     DOCTOR(::searchForDoctor),
     LPU(::searchForLpu),
     SERVICES(::searchForServices)
-}
-
-internal suspend fun searchForDoctor(
-    searchParams: SearchParamsNetwork,
-    orgsLinks: List<String>,
-    jsoupApi: IJsoupMapApi
-): List<TypedItemResponse> {
-
-    val results = mutableListOf<TypedItemResponse>()
-
-    orgsLinks.forEach { link ->
-
-        val doctorsResult = jsoupApi.getClinicDoctorsByQuery(
-            query = searchParams.query,
-            link = link
-        )
-
-        if (doctorsResult is NetworkRequestResult.Success) {
-            doctorsResult.data.forEach { doctor ->
-                doctor.apply {
-                    results.add(
-                        TypedItemResponse(
-                            title = fullName,
-                            subtitle = speciality,
-                            category = speciality,
-                            image = imageUri,
-                            link = uri
-                        )
-                    )
-                }
-            }
-        }
-    }
-
-    return results
-}
-
-
-internal suspend fun searchForLpu(
-    searchParams: SearchParamsNetwork,
-    orgsLinks: List<String>,
-    jsoupApi: IJsoupMapApi
-): List<TypedItemResponse> {
-
-    val results = mutableListOf<TypedItemResponse>()
-
-    orgsLinks.forEach { link ->
-
-        val clinicResult = jsoupApi.getClinicInfo(link)
-
-        if (clinicResult is NetworkRequestResult.Success)
-            clinicResult.data.apply {
-                results.add(
-                    TypedItemResponse(
-                        title = name,
-                        subtitle = address,
-                        category = type ?: "LPU",
-                        image = imageUri,
-                        link = link
-                    )
-                )
-            }
-    }
-
-    return results
-}
-
-
-internal suspend fun searchForServices(
-    searchParams: SearchParamsNetwork,
-    orgsLinks: List<String>,
-    jsoupApi: IJsoupMapApi
-): List<TypedItemResponse> {
-
-    val results = mutableListOf<TypedItemResponse>()
-
-    orgsLinks.forEach { link ->
-
-        val servicesRes =
-            jsoupApi.isContainingService(link, searchParams.query)
-
-        if (servicesRes is NetworkRequestResult.Success && servicesRes.data) {
-            val clinicResult = jsoupApi.getClinicInfo(link)
-
-            if (clinicResult is NetworkRequestResult.Success)
-                clinicResult.data.apply {
-                    results.add(
-                        TypedItemResponse(
-                            title = name,
-                            subtitle = address,
-                            category = type ?: "LPU",
-                            image = imageUri,
-                            link = link
-                        )
-                    )
-                }
-        }
-    }
-
-    return results
 }
