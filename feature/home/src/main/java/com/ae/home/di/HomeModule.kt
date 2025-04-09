f3a4f4cf47de3f7e04a104e5336d3f3e364145e4
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-09 20:15:17 +0300

    feat(feature:home): annotate with @HomeScope HomeModule providers

--- feature/home/src/main/java/com/ae/home/di/HomeModule.kt
+++ feature/home/src/main/java/com/ae/home/di/HomeModule.kt
@@ -14,6 +14,7 @@ import javax.inject.Singleton
 @Module
 internal class HomeModule {
 
+    @HomeScope
     @Provides
     fun provideSearchViewModelFactory(
         @DefaultDispatcher defaultDispatcher: CoroutineDispatcher,
