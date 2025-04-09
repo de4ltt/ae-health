71d1c3f9e5f8a39a50270d20fad2a7935da8ba3a
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-09 20:45:46 +0300

    feat(core:network): add NetworkRequestError

---
+++ core/network/src/main/java/com/ae/network/request_result/NetworkRequestError.kt
@@ -0,0 +1,15 @@
+package com.ae.network.request_result
+
+sealed class NetworkRequestError(message: String?) : Throwable() {
+    data class ClientError(override val message: String? = null) : NetworkRequestError(message) {
+        private fun readResolve(): Any = ClientError(message)
+    }
+
+    data class ServerError(override val message: String? = null) : NetworkRequestError(message) {
+        private fun readResolve(): Any = ServerError(message)
+    }
+
+    data class UnknownError(override val message: String? = null) : NetworkRequestError(message) {
+        private fun readResolve(): Any = UnknownError(message)
+    }
+}
