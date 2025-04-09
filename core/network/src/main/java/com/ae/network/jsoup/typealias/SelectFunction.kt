da3408a32e89fbb4324b7b71a4a978e2dc86a3bb
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-09 22:04:36 +0300

    feat(core:network): add SelectFunction typealias

---
+++ core/network/src/main/java/com/ae/network/jsoup/typealias/SelectFunction.kt
@@ -0,0 +1,6 @@
+package com.ae.network.jsoup.`typealias`
+
+import com.ae.network.dto.retrofit.TypedItemResponse
+import org.jsoup.nodes.Document
+
+internal typealias SelectFunction = suspend (Document) -> Pair<String, List<TypedItemResponse>>?
