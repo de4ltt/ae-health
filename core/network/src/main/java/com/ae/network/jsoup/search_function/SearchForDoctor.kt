32c19d0b6c7c2e0fdf6fdab423318d841d28cbed
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-09 20:52:39 +0300

    feat(core:network): add SearchForDoctor

---
+++ core/network/src/main/java/com/ae/network/jsoup/search_function/SearchForDoctor.kt
@@ -0,0 +1,41 @@
+package com.ae.network.jsoup.search_function
+
+import com.ae.network.dto.retrofit.TypedItemResponse
+import com.ae.network.jsoup.IJsoupMapApi
+import com.ae.network.model.SearchParamsNetwork
+import com.ae.network.request_result.NetworkRequestResult
+
+internal suspend fun searchForDoctor(
+    searchParams: SearchParamsNetwork,
+    orgsLinks: List<String>,
+    jsoupApi: IJsoupMapApi
+): List<TypedItemResponse> {
+
+    val results = mutableListOf<TypedItemResponse>()
+
+    orgsLinks.forEach { link ->
+
+        val doctorsResult = jsoupApi.getClinicDoctorsByQuery(
+            query = searchParams.query,
+            link = link
+        )
+
+        if (doctorsResult is NetworkRequestResult.Success) {
+            doctorsResult.data.forEach { doctor ->
+                doctor.apply {
+                    results.add(
+                        TypedItemResponse(
+                            title = fullName,
+                            subtitle = speciality,
+                            category = speciality,
+                            image = imageUri,
+                            link = uri
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    return results
+}
