148f3b3cb9d9a1cf42ffec93e1a86b5c886bfe16
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-10 11:41:15 +0300

    feat(core:network): add JsoupModule bindings

--- core/network/src/main/java/com/ae/network/di/JsoupModule.kt
+++ core/network/src/main/java/com/ae/network/di/JsoupModule.kt
@@ -1,6 +1,8 @@
 package com.ae.network.di
 
+import com.ae.network.jsoup.IJsoupFindApi
 import com.ae.network.jsoup.IJsoupMapApi
+import com.ae.network.jsoup.implementation.JsoupFindApi
 import com.ae.network.jsoup.implementation.JsoupMapApi
 import dagger.Binds
 import dagger.Module
@@ -10,6 +12,10 @@ internal abstract class JsoupModule {
 
     @NetworkScope
     @Binds
-    abstract fun bindJsoupApi(jsoupApi: JsoupMapApi): IJsoupMapApi
+    abstract fun bindJsoupMapApi(jsoupMapApi: JsoupMapApi): IJsoupMapApi
+
+    @NetworkScope
+    @Binds
+    abstract fun bindJsoupFindApi(jsoupFindApi: JsoupFindApi): IJsoupFindApi
 
 }
