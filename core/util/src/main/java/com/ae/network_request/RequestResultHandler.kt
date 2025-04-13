217c21552dbd2ff6f3742086f465a1440f200f66
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-13 17:08:25 +0300

    feat(core:util): add RequestResultHandler

---
+++ core/util/src/main/java/com/ae/network_request/RequestResultHandler.kt
@@ -0,0 +1,15 @@
+package com.ae.network_request
+
+import com.ae.network_request.NetworkRequestResult
+
+fun <T> NetworkRequestResult<T>.handleResult(): NetworkRequestResult<T> =
+    if (this is NetworkRequestResult.Success)
+        NetworkRequestResult.Success(this.data)
+    else
+        this as NetworkRequestResult.Error
+
+fun <T, V> NetworkRequestResult<T>.handleResult(transform: (T) -> V) =
+    if (this is NetworkRequestResult.Success)
+        NetworkRequestResult.Success(transform(this.data))
+    else
+        this as NetworkRequestResult.Error
