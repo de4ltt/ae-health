26126149a687ae6bc0d7568b8d982fe09e39e062
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-13 17:09:09 +0300

    refactor(app): remove unused imports

--- app/src/main/java/com/ae/health/MainActivity.kt
+++ app/src/main/java/com/ae/health/MainActivity.kt
@@ -1,28 +1,11 @@
 package com.ae.health
 
 import android.os.Bundle
-import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
 import com.ae.home.BasicBox
 import com.ae.home.viewmodel.SearchViewModel
-import com.ae.home.viewmodel.SearchViewModelFactory
-import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
 
--- core/util/src/main/java/com/ae/network_request/NetworkRequestResult.kt
+++ core/util/src/main/java/com/ae/network_request/NetworkRequestResult.kt
@@ -4,4 +4,3 @@ sealed class NetworkRequestResult<out T> {
     data class Success<out T>(val data: T) : NetworkRequestResult<T>()
     data class Error(val error: NetworkRequestError) : NetworkRequestResult<Nothing>()
 }
-
