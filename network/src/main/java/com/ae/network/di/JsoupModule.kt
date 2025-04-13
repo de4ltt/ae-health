f4236df198478ea11fda6ae590ad7539960989f3
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-04-13 17:48:55 +0300

    refactor(network): remove IJsoupMapApi binder

--- network/src/main/java/com/ae/network/di/JsoupModule.kt
+++ network/src/main/java/com/ae/network/di/JsoupModule.kt
@@ -10,10 +10,6 @@ internal abstract class JsoupModule {
 
     @NetworkScope
     @Binds
-    abstract fun bindJsoupMapApi(jsoupMapApi: JsoupMapApi): IJsoupMapApi
-
-    @NetworkScope
-    @Binds
     abstract fun bindJsoupFindApi(jsoupFindApi: JsoupFindApi): IJsoupFindApi
 
 }
