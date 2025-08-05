6a085ccf608f7a08846af2e5333ecfcce5124a92
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:14:18 +0300

    feat(catalog-service): add SpecialityRepository

---
+++ catalog_service/src/main/java/feo/health/catalog_service/repository/SpecialityRepository.java
@@ -0,0 +1,10 @@
+package feo.health.catalog_service.repository;
+
+import feo.health.catalog_service.entity.Speciality;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpecialityRepository extends JpaRepository<Speciality, Long> {
+    Optional<Speciality> findByNameIgnoreCase(String name);
+}
