675382dd8130c482ab39e12f83b29bb3160ba5da
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:20:38 +0300

    feat(catalog-service): add @AllArgsConstructor in DoctorController

--- catalog_service/src/main/java/feo/health/catalog_service/controller/DoctorController.java
+++ catalog_service/src/main/java/feo/health/catalog_service/controller/DoctorController.java
@@ -2,6 +2,7 @@ package feo.health.catalog_service.controller;
 
 import feo.health.catalog_service.dto.DoctorDto;
 import feo.health.catalog_service.service.DoctorService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/catalog/doctors")
+@AllArgsConstructor
 public class DoctorController {
 
     private final DoctorService doctorService;
-
-    @Autowired
-    public DoctorController(DoctorService doctorService) {
-        this.doctorService = doctorService;
-    }
 
     @GetMapping
     ResponseEntity<?> getDoctorInfo(@RequestParam String uri) {
