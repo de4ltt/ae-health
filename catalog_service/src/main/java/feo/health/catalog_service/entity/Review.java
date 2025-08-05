484d23e09c0f3f4f2c0145e8a10000303e803ced
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:07:28 +0300

    feat(catalog-service): add Review entity

---
+++ catalog_service/src/main/java/feo/health/catalog_service/entity/Review.java
@@ -0,0 +1,29 @@
+package feo.health.catalog_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Date;
+
+@Entity
+@Table(name = "reviews")
+@Data
+public class Review {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Doctor doctor;
+
+    @Column
+    private Date date;
+
+    @Column(length = 10000)
+    private String text;
+
+    @Column
+    private Byte rating;
+
+}
