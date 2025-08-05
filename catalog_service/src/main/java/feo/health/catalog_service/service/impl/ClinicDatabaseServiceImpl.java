4f8b986648923c427b9c09ec06ff95cbd964771b
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:16:40 +0300

    feat(catalog-service): add ClinicDatabaseServiceImpl

---
+++ catalog_service/src/main/java/feo/health/catalog_service/service/impl/ClinicDatabaseServiceImpl.java
@@ -0,0 +1,33 @@
+package feo.health.catalog_service.service.impl;
+
+import feo.health.catalog_service.dto.ClinicDto;
+import feo.health.catalog_service.entity.Clinic;
+import feo.health.catalog_service.repository.ClinicRepository;
+import feo.health.catalog_service.service.ClinicDatabaseService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class ClinicDatabaseServiceImpl implements ClinicDatabaseService {
+
+    private ClinicRepository clinicRepository;
+
+    @Override
+    public boolean isClinicPresentByUri(String uri) {
+        return clinicRepository.existsByUri(uri);
+    }
+
+    @Override
+    public Clinic saveClinic(Clinic clinic) {
+        return clinicRepository.save(clinic);
+    }
+
+    @Override
+    public Optional<Clinic> getClinicByUri(String uri) {
+        return clinicRepository.findByUri(uri);
+    }
+
+}
