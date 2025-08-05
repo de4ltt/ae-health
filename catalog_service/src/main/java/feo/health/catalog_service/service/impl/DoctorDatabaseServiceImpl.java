dca4663344ebd8b3be9167ddf2c4edf6fd19571a
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:17:11 +0300

    feat(catalog-service): add DoctorDatabaseServiceImpl

---
+++ catalog_service/src/main/java/feo/health/catalog_service/service/impl/DoctorDatabaseServiceImpl.java
@@ -0,0 +1,33 @@
+package feo.health.catalog_service.service.impl;
+
+import feo.health.catalog_service.dto.DoctorDto;
+import feo.health.catalog_service.entity.Doctor;
+import feo.health.catalog_service.repository.DoctorRepository;
+import feo.health.catalog_service.service.DoctorDatabaseService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class DoctorDatabaseServiceImpl implements DoctorDatabaseService {
+
+    private DoctorRepository doctorRepository;
+
+    @Override
+    public Doctor saveDoctor(Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
+
+    @Override
+    public boolean isDoctorPresentByUrl(String url) {
+        return doctorRepository.existsByUri(url);
+    }
+
+    @Override
+    public Optional<Doctor> getDoctorByUrl(String url) {
+        return doctorRepository.findByUri(url);
+    }
+
+}
