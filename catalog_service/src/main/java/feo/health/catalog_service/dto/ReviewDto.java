7ff5389c2a263a822f2a563b1779dbb5955ec6b8
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:08:23 +0300

    feat(catalog-service): add Review dto

---
+++ catalog_service/src/main/java/feo/health/catalog_service/dto/ReviewDto.java
@@ -0,0 +1,16 @@
+package feo.health.catalog_service.dto;
+
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
+public class ReviewDto {
+
+    private String text;
+
+    private Date date;
+
+    private Byte rating;
+
+}
