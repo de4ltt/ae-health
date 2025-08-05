fd2deb30dbf0b6d478b3ccc0752187e263779d44
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:08:12 +0300

    feat(catalog-service): add DoctorService dto

---
+++ catalog_service/src/main/java/feo/health/catalog_service/dto/DoctorsServiceDto.java
@@ -0,0 +1,14 @@
+package feo.health.catalog_service.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class DoctorsServiceDto {
+
+    private String title;
+
+    private BigDecimal price;
+
+}
