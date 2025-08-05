3d3431eb711f3b821d3f00c8370fab237229e641
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:11:19 +0300

    feat(catalog-service): add GeneralItemsHtmlClient

---
+++ catalog_service/src/main/java/feo/health/catalog_service/html/client/GeneralItemsHtmlClient.java
@@ -0,0 +1,17 @@
+package feo.health.catalog_service.html.client;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GeneralItemsHtmlClient {
+
+    public Document getGeneralItemsPage(String query) throws IOException {
+        final String url = String.format("https://prodoctorov.ru/krasnodar/find/?q=%s", query);
+        return Jsoup.connect(url).get();
+    }
+
+}
