339d93d78115bda6f697864c6672047a9c79210d
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:53:53 +0300

    feat(catalog-service): add getClinicInfo to ClinicController

--- catalog_service/src/main/java/feo/health/catalog_service/controller/ClinicController.java
+++ catalog_service/src/main/java/feo/health/catalog_service/controller/ClinicController.java
@@ -1,11 +1,28 @@
 package feo.health.catalog_service.controller;
 
+import feo.health.catalog_service.dto.ClinicDto;
+import feo.health.catalog_service.service.ClinicService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/catalog/clinics")
 @AllArgsConstructor
 public class ClinicController {
+
+    private final ClinicService clinicService;
+
+    @GetMapping
+    ResponseEntity<ClinicDto> getDoctorInfo(
+            @RequestParam String uri,
+            @RequestParam(defaultValue = "true") Boolean located
+    ) {
+        ClinicDto clinic = clinicService.getClinicInfo(uri, located);
+        return ResponseEntity.ok(clinic);
+    }
+
 }
