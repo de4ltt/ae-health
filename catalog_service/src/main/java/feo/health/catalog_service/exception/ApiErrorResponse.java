522676b868c5b02f93fa6485128b32779e60705d
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:09:29 +0300

    feat(catalog-service): add ApiErrorResponse

---
+++ catalog_service/src/main/java/feo/health/catalog_service/exception/ApiErrorResponse.java
@@ -0,0 +1,15 @@
+package feo.health.catalog_service.exception;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+public record ApiErrorResponse(
+        int status,
+        String message,
+        LocalDateTime timestamp
+) {
+    public static ApiErrorResponse of(HttpStatus status, String message) {
+        return new ApiErrorResponse(status.value(), message, LocalDateTime.now());
+    }
+}
