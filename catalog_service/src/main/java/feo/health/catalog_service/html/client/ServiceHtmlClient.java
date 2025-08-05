73b3363c2fbe1831b50c45c1f7ca270417b23de9
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:12:01 +0300

    feat(catalog-service): add ServiceHtmlClient

---
+++ catalog_service/src/main/java/feo/health/catalog_service/html/client/ServiceHtmlClient.java
@@ -0,0 +1,17 @@
+package feo.health.catalog_service.html.client;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class ServiceHtmlClient {
+
+    public Document getServicesPage(String query) throws IOException {
+        final String url = String.format("https://prodoctorov.ru/krasnodar/find/?q=%s&filter=services", query);
+        return Jsoup.connect(url).get();
+    }
+
+}
