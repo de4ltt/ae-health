3cc8b5c7fbc1a8e52dfda0b3d6738eb8a7102975
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:12:48 +0300

    feat(catalog-service): add DoctorMapper

---
+++ catalog_service/src/main/java/feo/health/catalog_service/mapper/DoctorMapper.java
@@ -0,0 +1,60 @@
+package feo.health.catalog_service.mapper;
+
+import feo.health.catalog_service.dto.*;
+import feo.health.catalog_service.entity.*;
+import feo.health.catalog_service.repository.ClinicRepository;
+import feo.health.catalog_service.repository.DoctorRepository;
+import feo.health.catalog_service.repository.SpecialityRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class DoctorMapper {
+
+    private final ClinicMapper clinicMapper;
+    private final SpecialityMapper specialityMapper;
+    private final ReviewMapper reviewMapper;
+    private final DoctorsServiceMapper doctorsServiceMapper;
+
+    public DoctorDto toDto(Doctor doctor) {
+
+        if (doctor == null) return null;
+
+        DoctorDto dto = new DoctorDto();
+        dto.setName(doctor.getName());
+        dto.setUri(doctor.getUri());
+        dto.setImageUri(doctor.getImageUri());
+        dto.setItemType("doctor");
+        dto.setExperience(doctor.getExperience());
+        dto.setSpecialities(specialityMapper.toDto(doctor.getSpecialities()));
+        dto.setReviews(reviewMapper.toDto(doctor.getReviews()));
+        dto.setServices(doctorsServiceMapper.toDto(doctor.getServices()));
+        dto.setClinics(clinicMapper.toDto(doctor.getClinics()));
+
+        return dto;
+    }
+
+    public Doctor toEntity(DoctorDto doctorDto) {
+
+        if (doctorDto == null) return null;
+
+        Doctor doctor = new Doctor();
+        doctor.setName(doctorDto.getName());
+        doctor.setUri(doctorDto.getUri());
+        doctor.setExperience(doctorDto.getExperience());
+        doctor.setImageUri(doctorDto.getImageUri());
+        doctor.setSpecialities(specialityMapper.toEntity(doctorDto.getSpecialities()));
+        doctor.setReviews(reviewMapper.toEntity(doctorDto.getReviews(), doctor));
+        doctor.setServices(doctorsServiceMapper.toEntity(doctorDto.getServices(), doctor));
+        doctor.setClinics(clinicMapper.toEntity(doctorDto.getClinics()));
+
+        return doctor;
+    }
+
+}
