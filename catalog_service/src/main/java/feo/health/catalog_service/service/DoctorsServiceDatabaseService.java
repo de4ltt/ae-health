675d817a6c5ca14d4a935d02dcc73c0e74024533
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:15:58 +0300

    feat(catalog-service): add DoctorsServiceDatabaseService

---
+++ catalog_service/src/main/java/feo/health/catalog_service/service/DoctorsServiceDatabaseService.java
@@ -0,0 +1,7 @@
+package feo.health.catalog_service.service;
+
+import feo.health.catalog_service.entity.DoctorsService;
+
+public interface DoctorsServiceDatabaseService {
+    DoctorsService saveDoctorService(DoctorsService doctorsService);
+}
