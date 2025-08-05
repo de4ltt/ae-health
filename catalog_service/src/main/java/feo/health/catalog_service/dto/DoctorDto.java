4e4ae019137a0f364fd3120b3cc60efbc7d96a9f
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:08:00 +0300

    feat(catalog-service): add Doctor dto

---
+++ catalog_service/src/main/java/feo/health/catalog_service/dto/DoctorDto.java
@@ -0,0 +1,33 @@
+package feo.health.catalog_service.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class DoctorDto {
+
+    private String name;
+
+    @NotNull
+    private String uri;
+
+    @NotNull
+    private List<SpecialityDto> specialities;
+
+    private Byte experience;
+
+    private String imageUri;
+
+    @NotNull
+    private String itemType;
+
+    private List<ReviewDto> reviews;
+
+    private List<DoctorsServiceDto> services;
+
+    private List<ClinicDto> clinics;
+
+}
