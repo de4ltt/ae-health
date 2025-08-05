60ee22f440ca63fa6f5385a839f4c8027f43a26a
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-05 19:10:42 +0300

    feat(catalog-service): add ClinicHtmlClient

---
+++ catalog_service/src/main/java/feo/health/catalog_service/html/client/ClinicHtmlClient.java
@@ -0,0 +1,21 @@
+package feo.health.catalog_service.html.client;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class ClinicHtmlClient {
+
+    public Document getClinicsPage(String query) throws IOException {
+        String uri = String.format("https://prodoctorov.ru/krasnodar/find/?q=%s&filter=lpus", query);
+        return Jsoup.connect(uri).get();
+    }
+
+    public Document getClinicTypesPage(String query) throws IOException {
+        String uri = String.format("https://prodoctorov.ru/krasnodar/find/?q=%s&filter=lputypes", query);
+        return Jsoup.connect(uri).get();
+    }
+}
