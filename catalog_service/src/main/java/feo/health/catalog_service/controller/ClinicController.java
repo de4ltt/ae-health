7234847e3a9b4eb76a3d28738ba05d215ec0e141
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-07 19:57:44 +0300

    feat(catalog-service): change endpoint uri in ClinicController

--- catalog_service/src/main/java/feo/health/catalog_service/controller/ClinicController.java
+++ catalog_service/src/main/java/feo/health/catalog_service/controller/ClinicController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/catalog/clinics")
+@RequestMapping("/api/v1/catalog/clinic")
 @AllArgsConstructor
 public class ClinicController {
 
