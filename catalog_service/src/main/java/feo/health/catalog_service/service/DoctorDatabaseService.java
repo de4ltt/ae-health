742a2824e26dc45a43b5541e6683f83a3501144f
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-07 22:12:11 +0300

    chore(catalog-service): remove unused imports from DoctorDatabaseService

--- catalog_service/src/main/java/feo/health/catalog_service/service/DoctorDatabaseService.java
+++ catalog_service/src/main/java/feo/health/catalog_service/service/DoctorDatabaseService.java
@@ -1,9 +1,5 @@
 package feo.health.catalog_service.service;
 
-import feo.health.catalog_service.dto.DoctorDto;
-import feo.health.catalog_service.dto.DoctorsServiceDto;
-import feo.health.catalog_service.dto.ReviewDto;
-import feo.health.catalog_service.dto.SpecialityDto;
 import feo.health.catalog_service.entity.Doctor;
 
 import java.util.List;
