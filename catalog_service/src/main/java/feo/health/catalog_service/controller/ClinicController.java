be2552976ed29405010c6a9d9f8deda15d7ea481
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-07 19:40:14 +0300

    feat(catalog-service): add getClinicDoctors to ClinicController

--- catalog_service/src/main/java/feo/health/catalog_service/controller/ClinicController.java
+++ catalog_service/src/main/java/feo/health/catalog_service/controller/ClinicController.java
@@ -1,7 +1,9 @@
 package feo.health.catalog_service.controller;
 
 import feo.health.catalog_service.dto.ClinicDto;
+import feo.health.catalog_service.dto.DoctorDto;
 import feo.health.catalog_service.service.ClinicService;
+import feo.health.catalog_service.service.DoctorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,20 +11,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/catalog/clinics")
 @AllArgsConstructor
 public class ClinicController {
 
     private final ClinicService clinicService;
+    private final DoctorService doctorService;
 
     @GetMapping
-    ResponseEntity<ClinicDto> getDoctorInfo(
+    ResponseEntity<ClinicDto> getClinicInfo(
             @RequestParam String uri,
             @RequestParam(defaultValue = "true") Boolean located
     ) {
         ClinicDto clinic = clinicService.getClinicInfo(uri, located);
         return ResponseEntity.ok(clinic);
+    }
+
+    @GetMapping("/doctors")
+    ResponseEntity<List<DoctorDto>> getClinicDoctors(@RequestParam String uri) {
+        List<DoctorDto> clinicDtos = doctorService.getClinicDoctors(uri);
+        return ResponseEntity.ok(clinicDtos);
     }
 
 }
