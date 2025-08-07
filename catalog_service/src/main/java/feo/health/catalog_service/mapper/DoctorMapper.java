e7e8ff38ca25e6a127ef416669e18ef7cc2d492b
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-07 19:43:24 +0300

    feat(catalog-service): add doctor rating check in DoctorMapper

--- catalog_service/src/main/java/feo/health/catalog_service/mapper/DoctorMapper.java
+++ catalog_service/src/main/java/feo/health/catalog_service/mapper/DoctorMapper.java
@@ -2,8 +2,11 @@ package feo.health.catalog_service.mapper;
 
 import feo.health.catalog_service.dto.*;
 import feo.health.catalog_service.entity.*;
+import feo.health.catalog_service.service.DoctorDatabaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -14,27 +17,37 @@ public class DoctorMapper {
     private final ReviewMapper reviewMapper;
     private final DoctorsServiceMapper doctorsServiceMapper;
 
+    private final DoctorDatabaseService doctorDatabaseService;
+
     public DoctorDto toDto(Doctor doctor) {
 
         if (doctor == null) return null;
 
-        DoctorDto dto = new DoctorDto();
-        dto.setName(doctor.getName());
-        dto.setUri(doctor.getUri());
-        dto.setImageUri(doctor.getImageUri());
-        dto.setItemType("doctor");
-        dto.setExperience(doctor.getExperience());
-        dto.setSpecialities(specialityMapper.toDto(doctor.getSpecialities()));
-        dto.setReviews(reviewMapper.toDto(doctor.getReviews()));
-        dto.setServices(doctorsServiceMapper.toDto(doctor.getServices()));
-        dto.setClinics(clinicMapper.toDto(doctor.getClinics()));
+        DoctorDto doctorDto = new DoctorDto();
+        doctorDto.setName(doctor.getName());
+        doctorDto.setUri(doctor.getUri());
+        doctorDto.setImageUri(doctor.getImageUri());
+        doctorDto.setItemType("doctor");
+        doctorDto.setExperience(doctor.getExperience());
+        doctorDto.setSpecialities(specialityMapper.toDto(doctor.getSpecialities()));
+        doctorDto.setReviews(reviewMapper.toDto(doctor.getReviews()));
+        doctorDto.setServices(doctorsServiceMapper.toDto(doctor.getServices()));
+        doctorDto.setClinics(clinicMapper.toDto(doctor.getClinics()));
+        doctorDto.setRating(doctor.getRating());
 
-        return dto;
+        return doctorDto;
     }
 
     public Doctor toEntity(DoctorDto doctorDto) {
 
         if (doctorDto == null) return null;
+
+        if (doctorDatabaseService.isDoctorPresentByUrl(doctorDto.getUri())) {
+            Doctor doctor = doctorDatabaseService.getDoctorByUrl(doctorDto.getUri()).get();
+            doctor.setRating(doctor.getRating() == null ? doctorDto.getRating() : doctor.getRating());
+            doctorDatabaseService.saveDoctor(doctor);
+            return doctor;
+        }
 
         Doctor doctor = new Doctor();
         doctor.setName(doctorDto.getName());
@@ -45,8 +58,16 @@ public class DoctorMapper {
         doctor.setReviews(reviewMapper.toEntity(doctorDto.getReviews(), doctor));
         doctor.setServices(doctorsServiceMapper.toEntity(doctorDto.getServices(), doctor));
         doctor.setClinics(clinicMapper.toEntity(doctorDto.getClinics()));
+        doctor.setRating(doctorDto.getRating());
 
         return doctor;
+    }
+
+    public List<Doctor> toEntity(List<DoctorDto> doctorDtos) {
+        return doctorDtos.stream()
+                .map(this::toEntity)
+                .filter(doctor -> doctor.getUri().contains("vrach"))
+                .toList();
     }
 
 }
