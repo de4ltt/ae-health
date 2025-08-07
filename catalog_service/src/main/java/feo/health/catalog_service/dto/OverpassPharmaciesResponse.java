8f235616efb22692e45f0b18a6e64d9ccb5a2365
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-07 22:15:13 +0300

    feat(catalog-service): add OverpassPharmaciesResponse

---
+++ catalog_service/src/main/java/feo/health/catalog_service/dto/OverpassPharmaciesResponse.java
@@ -0,0 +1,20 @@
+package feo.health.catalog_service.dto;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class OverpassPharmaciesResponse {
+
+    private List<Element> elements;
+
+    @Data
+    public static class Element {
+        private double lat;
+        private double lon;
+        private Map<String, String> tags;
+    }
+
+}
