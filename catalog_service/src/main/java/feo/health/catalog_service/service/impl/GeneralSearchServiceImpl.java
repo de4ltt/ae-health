6e21c9a68e3d24a7dda87bcae65ad9311edca466
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-07 19:44:30 +0300

    feat(catalog-service): add save to db in GeneralSearchService

--- catalog_service/src/main/java/feo/health/catalog_service/service/impl/GeneralSearchServiceImpl.java
+++ catalog_service/src/main/java/feo/health/catalog_service/service/impl/GeneralSearchServiceImpl.java
@@ -5,12 +5,17 @@ import feo.health.catalog_service.html.client.GeneralItemsHtmlClient;
 import feo.health.catalog_service.html.parser.ClinicHtmlParser;
 import feo.health.catalog_service.html.parser.DoctorHtmlParser;
 import feo.health.catalog_service.html.parser.ServiceHtmlParser;
+import feo.health.catalog_service.mapper.ClinicMapper;
+import feo.health.catalog_service.mapper.DoctorMapper;
+import feo.health.catalog_service.service.ClinicDatabaseService;
+import feo.health.catalog_service.service.DoctorDatabaseService;
 import feo.health.catalog_service.service.GeneralSearchService;
 import lombok.AllArgsConstructor;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -22,15 +27,28 @@ public class GeneralSearchServiceImpl implements GeneralSearchService {
     private final ServiceHtmlParser serviceHtmlParser;
     private final ClinicHtmlParser clinicHtmlParser;
 
+    private final DoctorDatabaseService doctorDatabaseService;
+    private final ClinicDatabaseService clinicDatabaseService;
+
+    private final DoctorMapper doctorMapper;
+    private final ClinicMapper clinicMapper;
+
     @Override
     public SearchDto search(String query, Boolean located) {
         try {
             Document generalItemsDocument = generalItemsHtmlClient.getGeneralItemsPage(query);
             SearchDto result = new SearchDto();
 
-            result.setDoctors(doctorHtmlParser.parseDoctorsAndSpecialities(generalItemsDocument));
-            result.setServices(serviceHtmlParser.parseServices(generalItemsDocument));
-            result.setClinics(clinicHtmlParser.parseClinicsAndClinicTypes(generalItemsDocument));
+            List<DoctorDto> doctorDtos = doctorHtmlParser.parseDoctorsAndSpecialities(generalItemsDocument);
+            List<ServiceDto> serviceDtos = serviceHtmlParser.parseServices(generalItemsDocument);
+            List<ClinicDto> clinicDtos = clinicHtmlParser.parseClinicsAndClinicTypes(generalItemsDocument);
+
+            doctorDatabaseService.saveDoctors(doctorMapper.toEntity(doctorDtos));
+            clinicDatabaseService.saveClinics(clinicMapper.toEntity(clinicDtos));
+
+            result.setDoctors(doctorDtos);
+            result.setServices(serviceDtos);
+            result.setClinics(clinicDtos);
 
             return result;
         } catch (IOException e) {
