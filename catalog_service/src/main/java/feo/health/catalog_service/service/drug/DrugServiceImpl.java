58e88780067c983ba44d12df62468b0276636693
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-08 19:40:26 +0300

    feat(catalog-service): add DrugServiceImpl

---
+++ catalog_service/src/main/java/feo/health/catalog_service/service/drug/DrugServiceImpl.java
@@ -0,0 +1,42 @@
+package feo.health.catalog_service.service.drug;
+
+import feo.health.catalog_service.dto.DrugDto;
+import feo.health.catalog_service.html.client.DrugHtmlClient;
+import feo.health.catalog_service.html.parser.DrugHtmlParser;
+import lombok.AllArgsConstructor;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class DrugServiceImpl implements DrugService {
+
+    final DrugHtmlParser parser;
+    final DrugHtmlClient client;
+
+    @Override
+    public List<DrugDto> searchDrugs(String query) {
+        try {
+            Document drugsDocument = client.getDrugsPage(query);
+            return parser.parseDrugs(drugsDocument);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public DrugDto getDrugInfo(String drugUri) {
+        try {
+            Document drugDocument = client.getDrugPage(drugUri);
+            DrugDto result = parser.parseDrug(drugDocument);
+            result.setUri(drugUri);
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
