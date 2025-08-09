ee840e43c0b58fe65e1e8ad53a73a206105eadc5
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-09 10:51:45 +0300

    feat(catalog-service): add DiseaseController

---
+++ catalog_service/src/main/java/feo/health/catalog_service/controller/DiseaseController.java
@@ -0,0 +1,31 @@
+package feo.health.catalog_service.controller;
+
+import feo.health.catalog_service.dto.DiseaseDto;
+import feo.health.catalog_service.service.disease.DiseaseService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/catalog/diseases")
+@AllArgsConstructor
+public class DiseaseController {
+
+    private final DiseaseService diseaseService;
+
+    @GetMapping
+    public ResponseEntity<List<DiseaseDto>> searchDiseases(@RequestParam String query) {
+        return ResponseEntity.ok(diseaseService.searchDiseases(query));
+    }
+
+    @GetMapping("/{uri}")
+    public ResponseEntity<String> getDiseaseInfo(@PathVariable String uri) {
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.TEXT_HTML)
+                .body(diseaseService.getDiseaseArticle(uri));
+    }
+}
