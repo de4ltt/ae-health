5c20e69e2b648793f579d176cc4062aa529c69f3
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-09 10:51:15 +0300

    feat(catalog-service): add DiseaseHtmlClient

---
+++ catalog_service/src/main/java/feo/health/catalog_service/html/client/DiseaseHtmlClient.java
@@ -0,0 +1,22 @@
+package feo.health.catalog_service.html.client;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class DiseaseHtmlClient {
+
+    public Document getDiseaseArticlePage(String nameUri) throws IOException {
+        String uri = String.format("https://probolezny.ru/%s/", nameUri);
+        return Jsoup.connect(uri).get();
+    }
+
+    public Document getDiseasesPage(String query) throws IOException {
+        String uri = String.format("https://probolezny.ru/?q=%s", query);
+        return Jsoup.connect(uri).get();
+    }
+
+}
