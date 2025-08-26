35a53e88e70654705ddc1286deacdd92e28376fe
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-26 11:48:01 +0300

    feat(gateway): add JwtAuthenticationFilter

---
+++ gateway/src/main/java/feo/health/gateway/security/JwtAuthenticationFilter.java
@@ -0,0 +1,73 @@
+package feo.health.gateway.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtAuthenticationFilter implements GlobalFilter {
+
+    private final SecretKey accessSecretKey;
+
+    public JwtAuthenticationFilter(@Value("${jwt.secret}") String secret) {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.accessSecretKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+
+        if (path.startsWith("/api/v1/auth"))
+            return chain.filter(exchange);
+
+        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+
+        try {
+            Claims claims = Jwts.parser()
+                    .verifyWith(accessSecretKey)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            String type = claims.get("type", String.class);
+            if (!"access".equals(type)) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            Long userId = claims.get("userId", Long.class);
+            String email = claims.getSubject();
+
+            exchange = exchange.mutate()
+                    .request(r -> r
+                            .header("X-User-Id", String.valueOf(userId))
+                            .header("X-User-Email", email)
+                    )
+                    .build();
+
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+}
