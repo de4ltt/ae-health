6a518f4ef795fd3ef8409e0a97402a8e935d3e2b
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-26 13:09:05 +0300

    chore(catalog-service): replace WebDriver with Browser client

--- catalog-service/src/main/java/feo/health/catalog_service/html/client/DoctorHtmlClient.java
+++ catalog-service/src/main/java/feo/health/catalog_service/html/client/DoctorHtmlClient.java
@@ -1,43 +1,38 @@
 package feo.health.catalog_service.html.client;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.PlaywrightException;
 import lombok.AllArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.time.Duration;
 
 @Component
 @AllArgsConstructor
 public class DoctorHtmlClient {
 
-    private final WebDriver webDriver;
+    private final Browser browser;
+
+    public Document getDoctorClinicsPage(String latinLink) {
+
+        String uri = String.format("https://prodoctorov.ru/krasnodar/vrach/%s", latinLink);
+
+        try (Page page = browser.newPage()) {
+            page.navigate(uri);
+            page.waitForSelector("#doctor-page-lpu-list");
+            String content = page.content();
+            return Jsoup.parse(content);
+        } catch (PlaywrightException e) {
+            throw new RuntimeException("Failed to load page " + uri, e);
+        }
+    }
 
     public Document getDoctorPage(String latinLink) throws IOException {
-        try {
-
-            String uri = String.format("https://prodoctorov.ru/krasnodar/vrach/%s", latinLink);
-
-            webDriver.get(uri);
-
-            new WebDriverWait(webDriver, Duration.ofSeconds(10))
-                    .until(ExpectedConditions.presenceOfElementLocated(
-                            By.cssSelector("div.doctor-page-list-lpu")));
-
-            String pageSource = webDriver.getPageSource();
-
-            if (pageSource == null)
-                throw new IOException("Couldn't retrieve " + uri);
-
-            return Jsoup.parse(pageSource);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        String uri = String.format("https://prodoctorov.ru/krasnodar/vrach/%s", latinLink);
+        return Jsoup.connect(uri).get();
     }
 
     public Document getDoctorsPage(String query) throws IOException {
