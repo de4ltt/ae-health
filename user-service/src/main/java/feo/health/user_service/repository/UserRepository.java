15843017213070f5e7c6e028f25f89c92f9e632c
Aleksej Timko <aleksejtmk@gmail.com>
2025-08-26 12:28:42 +0300

    feat(user-service): add UserService

---
+++ user-service/src/main/java/feo/health/user_service/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package feo.health.user_service.repository;
+
+import feo.health.user_service.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
